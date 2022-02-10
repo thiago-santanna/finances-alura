74b328ff6fe1773880c26a9c43c67d67545ef8c0
xthiagosant <uzmaki_thiago@outlook.com>
2022-02-10 14:01:39 -0300

    refatoracao filtro de receitas por usuario

--- src/main/java/com/tsswebapps/finance/service/receita/ListarTodasReceitasService.java
+++ src/main/java/com/tsswebapps/finance/service/receita/ListarTodasReceitasService.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 import com.tsswebapps.finance.dto.ReceitaDto;
 import com.tsswebapps.finance.model.Receita;
 import com.tsswebapps.finance.repository.IReceitaRepository;
+import com.tsswebapps.finance.service.user.BuscaUsuarioPorUserName;
+import com.tsswebapps.finance.service.user.UsuarioLogado;
 
 @Service
 public class ListarTodasReceitasService {
@@ -16,17 +18,22 @@ public class ListarTodasReceitasService {
 	@Autowired
 	private IReceitaRepository receitaRepository;
 	
+	@Autowired
+	private BuscaUsuarioPorUserName buscaUsuarioPorUserName;
+	
+	@Autowired
+	private UsuarioLogado usuarioLogado;
+	
 	public List<ReceitaDto> execute(String descricao){
 		List<Receita> receitas = null;
 		
-		if(descricao != null) {
-			receitas = receitaRepository.findByDescricaoContaining(descricao);
-		}
-		else{
-			receitas = receitaRepository.findAll();
-		}				
+		Long id = buscaUsuarioPorUserName.execute(usuarioLogado.execute()).getId();
+		
+		receitas = descricao != null ? receitaRepository.findByDescricaoContaining(descricao)
+				: receitaRepository.findAll();				
 		
 		List<ReceitaDto> receitasDto = receitas.stream()
+				.filter(desp -> desp.getUser().getId() == id)
 				.map(rec -> rec.toReceitaDto())
 				.collect(Collectors.toList());
 		
--- src/main/java/com/tsswebapps/finance/service/receita/ReceitasPorMesService.java
+++ src/main/java/com/tsswebapps/finance/service/receita/ReceitasPorMesService.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 import com.tsswebapps.finance.dto.ReceitaDto;
 import com.tsswebapps.finance.model.Receita;
 import com.tsswebapps.finance.repository.IReceitaRepository;
+import com.tsswebapps.finance.service.user.BuscaUsuarioPorUserName;
+import com.tsswebapps.finance.service.user.UsuarioLogado;
 
 @Service
 public class ReceitasPorMesService {
@@ -16,10 +18,20 @@ public class ReceitasPorMesService {
 	@Autowired
 	private IReceitaRepository repository;
 	
+	@Autowired
+	private BuscaUsuarioPorUserName buscaUsuarioPorUserName;
+	
+	@Autowired
+	private UsuarioLogado usuarioLogado;
+	
 	public List<ReceitaDto> execute(Integer ano, Integer mes){
+		
+		Long id = buscaUsuarioPorUserName.execute(usuarioLogado.execute()).getId();
+		
 		List<Receita> receitasPorMes = repository.findByPorMes(ano, mes);
 		
 		List<ReceitaDto> receitas = receitasPorMes.stream()
+				.filter(desp -> desp.getUser().getId() == id)
 				.map(rec -> rec.toReceitaDto())
 				.collect(Collectors.toList());
 		
