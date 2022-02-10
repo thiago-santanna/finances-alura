4215997bedb3ae2f378acd4bf92818e81084a4f8
xthiagosant <uzmaki_thiago@outlook.com>
2022-02-10 13:56:12 -0300

    refatoracao filtrando despesas por usuario

--- src/main/java/com/tsswebapps/finance/service/despesa/DespesasPorMesService.java
+++ src/main/java/com/tsswebapps/finance/service/despesa/DespesasPorMesService.java
@@ -9,17 +9,31 @@ import org.springframework.stereotype.Service;
 import com.tsswebapps.finance.dto.DespesaDto;
 import com.tsswebapps.finance.model.Despesa;
 import com.tsswebapps.finance.repository.IDespesaRepository;
+import com.tsswebapps.finance.service.user.BuscaUsuarioPorUserName;
+import com.tsswebapps.finance.service.user.UsuarioLogado;
 
 @Service
 public class DespesasPorMesService {
 	@Autowired
 	private IDespesaRepository repository;
 	
+	@Autowired
+	private BuscaUsuarioPorUserName buscaUsuarioPorUserName;
+	
+	@Autowired
+	private UsuarioLogado usuarioLogado;	
+	
 	public List<DespesaDto> execute(String ano, String mes){
+		
+		Long id = buscaUsuarioPorUserName.execute(usuarioLogado.execute()).getId();
+		
 		List<Despesa> despesasPorMes = repository.findByPorMes(ano, mes);
+		
 		List<DespesaDto> despesas = despesasPorMes.stream()
+				.filter(desp -> desp.getUser().getId() == id)
 				.map(desp -> desp.toDespesaDto())
 				.collect(Collectors.toList());
+		
 		return despesas;
 	}
 }
--- src/main/java/com/tsswebapps/finance/service/despesa/ListasTodasDespesasService.java
+++ src/main/java/com/tsswebapps/finance/service/despesa/ListasTodasDespesasService.java
@@ -9,28 +9,34 @@ import org.springframework.stereotype.Service;
 import com.tsswebapps.finance.dto.DespesaDto;
 import com.tsswebapps.finance.model.Despesa;
 import com.tsswebapps.finance.repository.IDespesaRepository;
+import com.tsswebapps.finance.service.user.BuscaUsuarioPorUserName;
+import com.tsswebapps.finance.service.user.UsuarioLogado;
 
 @Service
 public class ListasTodasDespesasService {
-	
+
 	@Autowired
 	private IDespesaRepository despesaRepository;
+
+	@Autowired
+	private BuscaUsuarioPorUserName buscaUsuarioPorUserName;
 	
-	public List<DespesaDto> execute(String descricao){
+	@Autowired
+	private UsuarioLogado usuarioLogado;
+	
+	public List<DespesaDto> execute(String descricao) {
 		List<Despesa> despesas = null;
+	
+		Long id = buscaUsuarioPorUserName.execute(usuarioLogado.execute()).getId();
 		
-		if(descricao != null) {
-			despesas = despesaRepository.findByDescricaoContaining(descricao);
-		}
-		else
-		{
-			despesas = despesaRepository.findAll();
-		}
-		
+		despesas = descricao != null ? despesaRepository.findByDescricaoContaining(descricao)
+				: despesaRepository.findAll();
+
 		List<DespesaDto> despesaDto = despesas.stream()
+				.filter(desp -> desp.getUser().getId() == id) //Isso nao sera performatico a longo prazo, mas pra esse projeto vai assim mesmo, o ideal seria fazer uma consulta personalizada no repositprio.
 				.map(desp -> desp.toDespesaDto())
 				.collect(Collectors.toList());
-		
+
 		return despesaDto;
 	}
 }
